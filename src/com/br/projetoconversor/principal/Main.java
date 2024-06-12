05bfc6595bf6b1e32fb53b88e05df714f460d945
Lucas Teixeira <lucasteixeira8784@gmail.com>
2024-06-11 22:55:01 -0300

    conexões HttpClient, HttpRequest e HttpResponde contruídas

--- src/com/br/projetoconversor/principal/Main.java
+++ src/com/br/projetoconversor/principal/Main.java
@@ -1,7 +1,12 @@
 package com.br.projetoconversor.principal;
 
+import com.br.projetoconversor.conversordemoedas.ConexaoApi;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ConexaoApi conexaoApi = new ConexaoApi();
+        conexaoApi.abrindorequisicao();
     }
 }
